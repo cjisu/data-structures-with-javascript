d26d7c53ca47e922ec7150b391b91e81ee6480c2
cjisu <h41469921@gmail.com>
2020-02-04 23:15:51 +0900

    modify hash table

--- hash_table.js
+++ hash_table.js
@@ -2,22 +2,40 @@ function HashTable() {
   // [] 로 하면 안됨 Array 파악 필요 함
   this.table = new Array(137);
   this.simpleHash = simpleHash;
+  this.betterHash = betterHash;
   this.showDistro = showDistro;
   this.put = put;
-  // this.get = get;
+  this.putWithSimple = putWithSimple;
+  this.get = get;
 }
 
-function simpleHash(data) {
+function simpleHash(key) {
   let total = 0;
-  for (let i = 0; i < data.length; i++) {
-    total += data.charCodeAt(i);
+  for (let i = 0; i < key.length; i++) {
+    total += key.charCodeAt(i);
   }
 
   return total % this.table.length;
 }
 
-function put(data) {
-  const pos = this.simpleHash(data);
+function betterHash(key) {
+  const H = 37;
+  let total = 0;
+  for (let i = 0; i < key.length; i++) {
+    total += H * total + key.charCodeAt(i);
+  }
+  // 최종 합계를 배열의 크기로 나눈 나머지 값
+  total = total % this.table.length;
+  return total;
+}
+
+function putWithSimple(key, data) {
+  const pos = this.simpleHash(key);
+  this.table[pos] = data;
+}
+
+function put(key, data) {
+  const pos = this.betterHash(key);
   this.table[pos] = data;
 }
 
@@ -27,9 +45,22 @@ function showDistro() {
   });
 }
 
-const hTable = new HashTable();
-hTable.put("sunset");
+function get(key) {
+  return this.table[this.betterHash(key)];
+}
+
+const hTableWithSimple = new HashTable();
+hTableWithSimple.putWithSimple("sunset", 1);
 // 충돌 테스트
-hTable.put("ssetun");
-hTable.put("noeul");
+hTableWithSimple.putWithSimple("ssetun", 2);
+hTableWithSimple.putWithSimple("noeul", 3);
+console.log("simple hash");
+hTableWithSimple.showDistro();
+
+const hTable = new HashTable();
+hTable.put("sunset", 1);
+hTable.put("ssetun", 2);
+hTable.put("noeul", 3);
+console.log("better hash");
 hTable.showDistro();
+console.log(hTable.get("sunset"));
