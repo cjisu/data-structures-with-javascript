83ac89546e2afcc9ae250bf989aa62f2a416b100
cjisu <h41469921@gmail.com>
2020-02-06 20:26:41 +0900

    modify graph test case

--- graph.js
+++ graph.js
@@ -85,10 +85,11 @@ function pathTo(v) {
 
 const g = new Graph(5);
 g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(1, 3);
+g.addEdge(1, 2);
+g.addEdge(2, 3);
 g.addEdge(2, 4);
+g.addEdge(3, 4);
 g.showGraph();
 // g.dfs(0);
 // g.bfs(4);
-g.pathTo(3);
+g.pathTo(4);
